21fd9c45e9b0cc978a535cf1428fd8e5ee8d2ba7
David Brawand <david.brawand@gmail.com>
2019-01-09 09:51:27 +0000

    minor edits

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,7 +5,7 @@
 set -g -x PATH /usr/local/bin $PATH
 set -g -x PATH ~/.scripts $PATH
 set -g -x PATH ~/.vim/plugged/vim-live-latex-preview/bin $PATH
-set -g -x PATH ~/.gem/ruby/2.5.0/bin $PATH
+#set -g -x PATH ~/.gem/ruby/2.5.0/bin $PATH
 set -g -x GOPATH ~/dev/go
 set -g -x SNAPPYHOME $HOME/snappy
 set -g -x SNAPPYWORK $HOME/work
