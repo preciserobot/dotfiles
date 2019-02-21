5e3d28b2cc4684ea02e5501d82ae8452c4555823
David Brawand <david.brawand@gmail.com>
2019-02-21 12:22:19 +0000

    changed GOPATH, added thefuck and direnv to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,7 +6,7 @@ set -g -x PATH /usr/local/bin $PATH
 set -g -x PATH ~/.scripts $PATH
 set -g -x PATH ~/.vim/plugged/vim-live-latex-preview/bin $PATH
 set -g -x PATH ~/.gem/ruby/2.5.0/bin $PATH
-set -g -x GOPATH ~/dev/go
+set -g -x GOPATH ~/Syncthing/private/go
 set -g -x SNAPPYHOME $HOME/snappy
 set -g -x SNAPPYWORK $HOME/work
 set -g -x SQVDAPIUSER admin
@@ -15,4 +15,9 @@ set -g -x EDITOR vim
 # set -g -x BROWSER 'firefox-developer-edition'
 # set -g -x BROWSER 'google-chrome-stable'
 set -g -x BROWSER 'brave'
-thefuck --alias | source
+
+# autocorrect
+thefuck --alias | .
+
+# direnv
+eval (direnv hook fish)
